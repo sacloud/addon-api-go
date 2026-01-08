8c506bfccb4dade7759d9797debb98090a109806
卜部 昌平 <s-urabe@sakura.ad.jp>
2026-01-08 14:42:00 +0900

    initial implementation of `Client`
    Signed-off-by: 卜部 昌平 <s-urabe@sakura.ad.jp>

---
+++ client.go
@@ -0,0 +1,48 @@
+// Copyright 2025- The sacloud/addon-api-go Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package addon
+
+import (
+	"fmt"
+	"runtime"
+
+	v1 "github.com/sacloud/addon-api-go/apis/v1"
+	"github.com/sacloud/saclient-go"
+)
+
+// DefaultAPIRootURL デフォルトのAPIルートURL
+const DefaultAPIRootURL = "https://secure.sakura.ad.jp/cloud/api/addon/1.0/"
+
+// UserAgent APIリクエスト時のユーザーエージェント
+var UserAgent = fmt.Sprintf(
+	"addon-api-go/%s (%s/%s; +https://github.com/sacloud/addon-api-go)",
+	Version,
+	runtime.GOOS,
+	runtime.GOARCH,
+)
+
+func NewClient(client saclient.ClientAPI) (*v1.Client, error) {
+	return NewClientWithAPIRootURL(client, DefaultAPIRootURL)
+}
+
+func NewClientWithAPIRootURL(client saclient.ClientAPI, apiRootURL string) (*v1.Client, error) {
+	if dupable, ok := client.(saclient.ClientOptionAPI); !ok {
+		return nil, NewError("client does not implement saclient.ClientOptionAPI", nil)
+	} else if augmented, err := dupable.DupWith(saclient.WithUserAgent(UserAgent)); err != nil {
+		return nil, err
+	} else {
+		return v1.NewClient(apiRootURL, v1.WithClient(augmented))
+	}
+}
